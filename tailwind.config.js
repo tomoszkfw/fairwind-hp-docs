8465783fe052d2327f6c0c4152943f82c492c2f3
tomoszkfw <160325821+tomoszkfw@users.noreply.github.com>
2024-07-11 20:15:57 +0900

    feat: NextUI のコンポーネントをスタイリング

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     content: [
         "./pages/**/*.{js,jsx,ts,tsx,md,mdx}",
         "./components/**/*.{js,jsx,ts,tsx,md,mdx}",
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         extend: {},

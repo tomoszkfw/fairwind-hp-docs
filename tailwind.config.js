c49e0c6c3f16a27962b44852a20ea074c7675889
tomoszkfw <160325821+tomoszkfw@users.noreply.github.com>
2024-07-11 20:21:31 +0900

    fix: NextUI を削除

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
     content: [
         "./pages/**/*.{js,jsx,ts,tsx,md,mdx}",
         "./components/**/*.{js,jsx,ts,tsx,md,mdx}",
-        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         extend: {},
